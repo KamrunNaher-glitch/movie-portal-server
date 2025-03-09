1a19b704f46e8a4aa239bd4f17fc475ffce0eba0
KamrunNaher-glitch <keyachandani7@gmail.com>
2025-03-09 17:10:09 +0600

    database operation done

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const movieCollection = client.db('moviePortal').collection('movie')
+    const movieCollection = client.db('moviePortal').collection('movie');
+    const userCollection = client.db('moviePortal').collection('users');
 
     app.get('/movie',async(req,res) =>{
       const cursor = movieCollection.find();
@@ -89,8 +90,39 @@ async function run() {
       res.send(result);
     })
 
+    // Users Related API 
 
+    app.get('/users',async (req,res) =>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
+      app.post('/users',async(req,res) => {
+        const newUser = req.body;
+        console.log('creating new user',newUser);
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+      })
+
+      app.patch('/users',async (req,res) => {
+        const email = req.body.email;
+        const filter = {email};
+        const updateDoc ={
+          $set : {
+            lastSignInTime : req.body?.lastSignInTime
+          }
+        }
+        const result = await userCollection.updateOne(filter,updateDoc);
+        res.send(result);
+      })
+
+      app.delete('/users/:id',async(req,res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await userCollection.deleteOne(query);
+        res.send(result);
+      })
 
 
 
